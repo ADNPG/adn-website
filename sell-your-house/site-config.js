609c3191d5a6b4994561ba7dbbe950f64dca8fb6
andre <andre@adnpropertygroup.com>
2026-07-22 20:55:14 -0400

    Wire seller intake endpoint

--- sell-your-house/site-config.js
+++ sell-your-house/site-config.js
@@ -1,3 +1,3 @@
 window.ADN_SELLER_SITE_CONFIG = {
-  formEndpoint: ""
+  formEndpoint: "https://script.google.com/macros/s/AKfycbw_tSZdwag3ba4HKvpJTOuNBSKvFt5GaOQ2vS7N4TaepNVXosdHCNIcRrJA9yxE4zC2cA/exec?route=seller-intake"
 };
